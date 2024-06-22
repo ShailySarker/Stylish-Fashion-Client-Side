b9deed50b31a0c086cceddbf9a720a1640b86293
ShailySarker <shailysarker99@gmail.com>
2024-06-22 15:44:55 +0600

    OurProducts section added with responsive design

---
+++ src/pages/Home/Components/OurProducts.jsx
@@ -0,0 +1,83 @@
+import product1 from "../../../assets/Images/Home/OurProducts_menCap.jpg";
+import product2 from "../../../assets/Images/Home/OurProducts_menHudi.jpg";
+import product3 from "../../../assets/Images/Home/OurProducts_menJacket.jpg";
+import product4 from "../../../assets/Images/Home/OurProducts_menPant.jpg";
+import product5 from "../../../assets/Images/Home/OurProducts_menShirt.jpg";
+import product6 from "../../../assets/Images/Home/OurProducts_menShorts.jpg";
+import product7 from "../../../assets/Images/Home/OurProducts_menSuit.jpg";
+import product8 from "../../../assets/Images/Home/OurProducts_menTshirt.jpg";
+import product9 from "../../../assets/Images/Home/OurProducts_menWinterCoat.jpg";
+import product10 from "../../../assets/Images/Home/OurProducts_womenBardotDress.jpg";
+import product11 from "../../../assets/Images/Home/OurProducts_womenFormal.jpg";
+import product12 from "../../../assets/Images/Home/OurProducts_womenMaxiCutDress.jpg";
+import product13 from "../../../assets/Images/Home/OurProducts_womenPantDress.jpg";
+import product14 from "../../../assets/Images/Home/OurProducts_womenShiftDress.jpg";
+import product15 from "../../../assets/Images/Home/OurProducts_womenSkirtDress.jpg";
+import product16 from "../../../assets/Images/Home/OurProducts_womenTopDress.jpg";
+import product17 from "../../../assets/Images/Home/OurProducts_womenWinterCap.jpg";
+import product18 from "../../../assets/Images/Home/OurProducts_womenWinterCoatDress.jpg";
+import product19 from "../../../assets/Images/Home/OurProducts_boyCap.jpg";
+import product20 from "../../../assets/Images/Home/OurProducts_boySuit.jpg";
+import product21 from "../../../assets/Images/Home/OurProducts_boyTshirtSet.jpg";
+import product22 from "../../../assets/Images/Home/OurProducts_boyWinterDress.jpg";
+import product23 from "../../../assets/Images/Home/OurProducts_girlCap.jpg";
+import product24 from "../../../assets/Images/Home/OurProducts_girlFrog.jpg";
+import product25 from "../../../assets/Images/Home/OurProducts_girlSkirtSet.jpg";
+import product26 from "../../../assets/Images/Home/OurProducts_girlWinterDress.jpg";
+import { useState } from "react";
+
+const OurProducts = () => {
+    const [seeAllProduct, setAllProduct] = useState(false);
+    const handleSeeMoreCollections = () => {
+        setAllProduct(!seeAllProduct);
+    };
+
+    return (
+        <div className="lg:px-20 md:px-12 px-6 lg:mt-20 md:mt-16 mt-14">
+            <div>
+                <h1 className="lg:text-4xl/normal md:text-3xl/normal text-2xl/normal font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-900">Our Products</h1>
+                <p className="text-center lg:w-[60%] md:w-[75%] w-[90%] mx-auto lg:text-xl/relaxed md:text-lg/relaxed text-base/relaxed lg:mt-4 md:mt-3 mt-2 text-black">Explore our extensive selection and enjoy a seamless shopping experience at Stylish Fashion!</p>
+            </div>
+            <div className="grid lg:grid-cols-6 md:grid-cols-4 grid-cols-3 lg:gap-6 md:gap-5 gap-4 lg:mt-12 md:mt-10 mt-7">
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product1} alt="product1" />
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product2} alt="product2" />
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product3} alt="product3" />
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product4} alt="product4" />
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product5} alt="product5" />
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product6} alt="product6" />
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product7} alt="product7" />
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product8} alt="product8" />
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product9} alt="product9" />
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product10} alt="product10" />
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product11} alt="product11" />
+                <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product12} alt="product12" />
+                {
+                    seeAllProduct && <>
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product13} alt="product13" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product14} alt="product14" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product15} alt="product15" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product16} alt="product16" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product17} alt="product17" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product18} alt="product18" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product19} alt="product19" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product20} alt="product20" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product21} alt="product21" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product22} alt="product22" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product23} alt="product23" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product24} alt="product24" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product25} alt="product25" />
+                        <img className="border-2 border-purple-800 rounded-xl lg:h-56 md:h-48 h-40 w-full shadow-lg" src={product26} alt="product26" />
+                    </>
+                }
+            </div>
+            <button className="bg-gradient-to-r from-blue-600 to-purple-800 text-white lg:w-56 md:w-48 w-44 py-2 rounded-2xl lg:text-lg text-base font-semibold flex justify-center mx-auto lg:mt-14 md:mt-12 mt-10" onClick={handleSeeMoreCollections}>
+                {
+                    seeAllProduct ?
+                        "See Less Products" : "See More Products"
+                }
+            </button>
+        </div >
+    );
+};
+
+export default OurProducts;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "./Components/Banner";
 import Category from "./Components/Category";
+import OurProducts from "./Components/OurProducts";
 
 const Home = () => {
     return (
         <>
             <Banner />
             <Category />
+            <OurProducts/>
         </>
     );
 };
