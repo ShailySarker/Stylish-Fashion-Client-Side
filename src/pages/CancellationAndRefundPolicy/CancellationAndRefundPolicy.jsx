35abb1a8f5c12cca11c86bf89c3817cea1285324
ShailySarker <shailysarker99@gmail.com>
2024-06-30 01:34:06 +0600

    CancellationAndRefundPolicy page added with responsive design

---
+++ src/pages/CancellationAndRefundPolicy/CancellationAndRefundPolicy.jsx
@@ -0,0 +1,83 @@
+const CancellationAndRefundPolicy = () => {
+    return (
+        <div>
+            <div className='bg-purple-800 lg:py-48 md:py-28 py-20'>
+                <h1 className='lg:text-8xl/relaxed md:text-7xl/normal text-4xl/relaxed text-white font-semibold text-center'>Cancellation and Refund Policy</h1>
+            </div>
+            <div className="lg:px-28 md:px-12 px-6 lg:pt-20 md:pt-14 pt-10 text-justify">
+                <div className="lg:pb-4 md:pb-2 pb-1">
+                    <div className="text-black flex flex-col lg:gap-10 md:gap-7 gap-5">
+                        <h3 className="lg:text-4xl md:text-3xl text-2xl font-semibold text-purple-800 text-left">Cancellation and Refund Policy</h3>
+                        <p className="lg:text-xl/relaxed md:text-lg/relaxed text-base/relaxed">At <span className="font-semibold">Stylish Fashion</span>, we strive to provide our customers with the best shopping experience. We understand that sometimes you may need to cancel or return an order. This Cancellation and Refund Policy outlines the terms and conditions for cancellations and refunds.</p>
+                    </div>
+                    <div className="lg:mt-12 md:mt-10 mt-8 text-black flex flex-col lg:gap-10 md:gap-7 gap-5">
+                        <h3 className="lg:text-3xl md:text-2xl text-xl font-semibold text-purple-800">1. Order Cancellation</h3>
+                        <div className="flex flex-col lg:gap-5 md:gap-4 gap-3">
+                            <h3 className="lg:text-2xl md:text-xl text-lg font-semibold text-purple-800">1.1 Before Shipment:</h3>
+                            <div>
+                                <ul className='flex flex-col gap-[10px] list-disc lg:pl-16 md:pl-10 pl-7 lg:text-xl/relaxed md:text-lg/relaxed text-base/relaxed'>
+                                    <li className=''>If you wish to cancel an order before it has been shipped, please contact our customer service team as soon as possible. We will make every effort to accommodate your request.</li>
+                                    <li className=''>If the order has not yet been processed for shipment, we will cancel it and issue a full refund.</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="flex flex-col lg:gap-5 md:gap-4 gap-3">
+                            <h3 className="lg:text-2xl md:text-xl text-lg font-semibold text-purple-800">1.2 After Shipment:</h3>
+                            <div>
+                                <ul className='flex flex-col gap-[10px] list-disc lg:pl-16 md:pl-10 pl-7 lg:text-xl/relaxed md:text-lg/relaxed text-base/relaxed'>
+                                    <li className=''>If your order has already been shipped, you may return the product(s) following our return policy outlined below.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:mt-12 md:mt-10 mt-8 text-black flex flex-col lg:gap-10 md:gap-7 gap-5">
+                        <h3 className="lg:text-3xl md:text-2xl text-xl font-semibold text-purple-800">2. Return and Refund Policy</h3>
+                        <div className="flex flex-col lg:gap-5 md:gap-4 gap-3">
+                            <h3 className="lg:text-2xl md:text-xl text-lg font-semibold text-purple-800">2.1 Eligibility:</h3>
+                            <div>
+                                <ul className='flex flex-col gap-[10px] list-disc lg:pl-16 md:pl-10 pl-7 lg:text-xl/relaxed md:text-lg/relaxed text-base/relaxed'>
+                                    <li className=''>We accept returns for most products within 30 days of delivery. The product(s) must be in their original condition, unused, unwashed, and with all original tags and packaging intact.</li>
+                                    <li className=''>For hygiene reasons, certain items such as undergarments, swimwear, and earrings are not eligible for return.</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="flex flex-col lg:gap-5 md:gap-4 gap-3">
+                            <h3 className="lg:text-2xl md:text-xl text-lg font-semibold text-purple-800">2.2 Return Process:</h3>
+                            <div>
+                                <ul className='flex flex-col gap-[10px] list-disc lg:pl-16 md:pl-10 pl-7 lg:text-xl/relaxed md:text-lg/relaxed text-base/relaxed'>
+                                    <li className=''>To initiate a return, please contact our customer service team to obtain a Return Merchandise Authorization (RMA) number and detailed return instructions.</li>
+                                    <li className=''>Pack the product(s) securely and include the RMA number inside the package.</li>
+                                    <li className=''>Ship the package to the address provided in the return instructions. We recommend using a trackable shipping method to ensure the package is returned to us safely.</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="flex flex-col lg:gap-5 md:gap-4 gap-3">
+                            <h3 className="lg:text-2xl md:text-xl text-lg font-semibold text-purple-800">2.3 Refund Process:</h3>
+                            <div>
+                                <ul className='flex flex-col gap-[10px] list-disc lg:pl-16 md:pl-10 pl-7 lg:text-xl/relaxed md:text-lg/relaxed text-base/relaxed'>
+                                    <li className=''>Once we receive and inspect the returned product(s), we will process your refund within 7-10 business days.</li>
+                                    <li className=''>Refunds will be issued to the original payment method used for the purchase. Depending on your payment provider, it may take additional time for the refund to appear in your account.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:mt-12 md:mt-10 mt-8 text-black flex flex-col lg:gap-5 md:gap-4 gap-3">
+                        <h3 className="lg:text-3xl md:text-2xl text-xl font-semibold text-purple-800">3. Exchange Policy</h3>
+                        <p className="lg:text-xl/relaxed md:text-lg/relaxed text-base/relaxed">We do not offer direct exchanges. If you wish to exchange a product, please follow the return process to obtain a refund and place a new order for the desired product.</p>
+                    </div>
+                    <div className="lg:mt-12 md:mt-10 mt-8 text-black flex flex-col lg:gap-5 md:gap-4 gap-3">
+                        <h3 className="lg:text-3xl md:text-2xl text-xl font-semibold text-purple-800">4. Damaged or Defective Products</h3>
+                        <p className="lg:text-xl/relaxed md:text-lg/relaxed text-base/relaxed">If you receive a damaged or defective product, please contact our customer service team within 7 days of delivery. We will arrange for a replacement or a full refund, including any shipping charges.</p>
+                    </div>
+                    <div className="lg:mt-12 md:mt-10 mt-8 text-black flex flex-col lg:gap-5 md:gap-4 gap-3">
+                        <h3 className="lg:text-3xl md:text-2xl text-xl font-semibold text-purple-800">6. Contact Us</h3>
+                        <p className="lg:text-xl/relaxed md:text-lg/relaxed text-base/relaxed">At <span className="font-semibold">Stylish Fashion</span>, customer satisfaction is our top priority. We offer excellent customer service, secure shopping, and a hassle-free return policy to ensure a smooth and enjoyable shopping experience.</p>
+                    </div>
+                    <p className="lg:text-xl/relaxed md:text-lg/relaxed text-base/relaxed lg:mt-12 md:mt-10 mt-8 text-black ">By making a purchase on our website, you acknowledge that you have read, understood, and agree to this Cancellation and Refund Policy. Thank you for shopping with <span className="font-semibold">Stylish Fashion</span>.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CancellationAndRefundPolicy;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -14,6 +14,7 @@ import ContactUs from "../pages/ContactUs/ContactUs";
 import AboutUs from "../pages/AboutUs/AboutUs";
 import TermsAndConditions from "../pages/TermsAndConditions/TermsAndConditions";
 import PrivacyPolicy from "../pages/PrivacyPolicy/PrivacyPolicy";
+import CancellationAndRefundPolicy from "../pages/CancellationAndRefundPolicy/CancellationAndRefundPolicy";
 
 const router = createBrowserRouter(
     [
@@ -68,6 +69,10 @@ const router = createBrowserRouter(
                 {
                     path: "/privacyPolicy",
                     element: <PrivacyPolicy />
+                },
+                {
+                    path: "/cancellationAndRefundPolicy",
+                    element: <CancellationAndRefundPolicy />
                 },
             ]
         },
