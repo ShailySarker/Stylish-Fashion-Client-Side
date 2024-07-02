a143f9447789753bd2186fd03b55efc0688465e6
ShailySarker <shailysarker99@gmail.com>
2024-07-03 00:14:59 +0600

    WomenFashion page updated with responsive design

--- src/pages/WomenFashion/WomenFashion.jsx
+++ src/pages/WomenFashion/WomenFashion.jsx
@@ -1,9 +1,13 @@
+import FAQs from "../../components/FAQs";
+import Newsletter from "../../components/Newsletter";
 import WomenProducts from "./Components/WomenProducts";
 
 const WomenFashion = () => {
     return (
         <>
             <WomenProducts />
+            <Newsletter />
+            <FAQs />
         </>
     );
 };
