030b21f3fe9c53d2e8601f4149cb280dd4a63641
ShailySarker <shailysarker99@gmail.com>
2024-07-02 23:35:40 +0600

    MenFashion page updated with responsive design

--- src/pages/MenFashion/MenFashion.jsx
+++ src/pages/MenFashion/MenFashion.jsx
@@ -1,9 +1,13 @@
+import FAQs from "../../components/FAQs";
+import Newsletter from "../../components/Newsletter";
 import MenProducts from "./Components/MenProducts";
 
 const MenFashion = () => {
     return (
         <>
             <MenProducts />
+            <Newsletter />
+            <FAQs />
         </>
     );
 };
