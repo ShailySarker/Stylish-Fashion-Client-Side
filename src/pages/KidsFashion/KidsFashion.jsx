66d4ef3d58690a68415166fcd40782ab06235378
ShailySarker <shailysarker99@gmail.com>
2024-07-09 00:53:56 +0600

    loader added for KidsProducts page components

--- src/pages/KidsFashion/KidsFashion.jsx
+++ src/pages/KidsFashion/KidsFashion.jsx
@@ -1,13 +1,21 @@
-import FAQs from "../../components/FAQs";
-import Newsletter from "../../components/Newsletter";
-import KidsProducts from "./Components/KidsProducts";
+import React, { Suspense } from "react";
+import LazyLoaderComponent from "../../components/LazyLoaderComponent";
+const FAQs = React.lazy(() => import("../../components/FAQs"));
+const Newsletter = React.lazy(() => import("../../components/Newsletter"));
+const KidsProducts = React.lazy(() => import("./Components/KidsProducts"));
 
 const KidsFashion = () => {
     return (
         <>
-            <KidsProducts />
-            <Newsletter />
-            <FAQs />
+            <Suspense fallback={<LazyLoaderComponent />}>
+                <KidsProducts />
+            </Suspense>
+            <Suspense fallback={<LazyLoaderComponent />}>
+                <Newsletter />
+            </Suspense>
+            <Suspense fallback={<LazyLoaderComponent />}>
+                <FAQs />
+            </Suspense>
         </>
     );
 };
