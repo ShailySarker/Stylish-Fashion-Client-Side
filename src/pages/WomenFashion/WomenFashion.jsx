86460fdb72c0fdc9f91203e597181934824749d2
ShailySarker <shailysarker99@gmail.com>
2024-07-09 01:11:18 +0600

    loader added for WomenFashion page components

--- src/pages/WomenFashion/WomenFashion.jsx
+++ src/pages/WomenFashion/WomenFashion.jsx
@@ -1,13 +1,21 @@
-import FAQs from "../../components/FAQs";
-import Newsletter from "../../components/Newsletter";
-import WomenProducts from "./Components/WomenProducts";
+import React, { Suspense } from "react";
+import LazyLoaderComponent from "../../components/LazyLoaderComponent";
+const FAQs = React.lazy(() => import("../../components/FAQs"));
+const Newsletter = React.lazy(() => import("../../components/Newsletter"));
+const WomenProducts = React.lazy(() => import("./Components/WomenProducts"));
 
 const WomenFashion = () => {
     return (
         <>
-            <WomenProducts />
-            <Newsletter />
-            <FAQs />
+            <Suspense fallback={<LazyLoaderComponent />}>
+                <WomenProducts />
+            </Suspense>
+            <Suspense fallback={<LazyLoaderComponent />}>
+                <Newsletter />
+            </Suspense>
+            <Suspense fallback={<LazyLoaderComponent />}>
+                <FAQs />
+            </Suspense>
         </>
     );
 };
