45a8bf8ed06a89a90f39bfea86ef0d3e18f8f752
ShailySarker <shailysarker99@gmail.com>
2024-08-02 15:41:38 +0600

    MyAccount page updated

--- src/pages/MyAccount/MyAccount.jsx
+++ src/pages/MyAccount/MyAccount.jsx
@@ -1,8 +1,12 @@
 
 const MyAccount = () => {
     return (
-        <div>
-            <h2 className="lg:text-2xl/relaxed md:text-xl/relaxed text-lg/relaxed lg:my-40 md:my-28 my-20 text-black text-center font-semibold">Coming Soon!</h2>
+        <div className="lg:px-20 md:px-12 px-6 lg:mt-5 md:mt-4 mt-3">
+            <div>
+                <h1 className="lg:text-4xl/normal md:text-3xl/normal text-2xl/normal font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-900">My Account</h1>
+                {/* <h1 className="lg:text-4xl/normal md:text-3xl/normal text-2xl/normal font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-900">My Selecting Product</h1> */}
+                <p className="lg:text-2xl/relaxed md:text-xl/relaxed text-lg/relaxed lg:my-48 md:my-40 my-32 text-black text-center font-semibold">Coming Soon!</p>
+            </div>
         </div>
     );
 };
