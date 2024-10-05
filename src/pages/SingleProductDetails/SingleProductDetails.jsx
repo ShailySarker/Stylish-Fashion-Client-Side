ba127429a3b62939c29d69a6b530cd57a9054752
ShailySarker <shailysarker99@gmail.com>
2024-10-05 23:25:48 +0600

    SingleProductDetails page functionality updated

--- src/pages/SingleProductDetails/SingleProductDetails.jsx
+++ src/pages/SingleProductDetails/SingleProductDetails.jsx
@@ -14,6 +14,8 @@ const SingleProductDetails = () => {
     const id = location.pathname.split("/")[2];
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     // Get user info and cart data from Redux store
     const currentUser = useSelector((state) => state?.user?.currentUser);
     // console.log("Current User:", currentUser);
@@ -30,11 +32,14 @@ const SingleProductDetails = () => {
     useEffect(() => {
         const getProduct = async () => {
             try {
+                setLoading(true);
                 const res = await publicRequest.get("/products/find/" + id);
                 console.log(res?.data);
                 setProduct(res?.data);
             } catch (error) {
-                console.log(error);
+                setError("Failed to fetch product");
+            } finally {
+                setLoading(false);
             }
         };
         getProduct();
@@ -101,65 +106,78 @@ const SingleProductDetails = () => {
     };
 
     return (
-        <div className="flex md:flex-row flex-col items-center lg:gap-8 md:gap-7 gap-6 lg:px-20 md:px-12 px-6 lg:mt-5 md:mt-4 mt-3">
-            <div className="lg:w-[40%] md:w-[35%]">
-                <img className="lg:h-96 md:h-64 h-52" src={product?.image} alt="product image" />
-            </div>
-            <div className="lg:w-[60%] md:w-[65%]">
-                <div>
-                    <h2 className="lg:text-4xl/normal md:text-3xl/normal text-2xl/normal font-bold text-black">{product?.title}</h2>
-                    <p className="lg:mt-3 md:mt-2 mt-1 lg:text-lg/normal text-justify">{product?.desc}</p>
-                    <h3 className="lg:text-4xl md:text-3xl text-2xl font-semibold text-black lg:mt-7 md:mt-5 mt-4">$ {product?.price}</h3>
-                </div>
-                <div className="lg:mt-7 md:mt-5 mt-4 flex md:flex-row md:flex-nowrap flex-wrap lg:gap-16 md:gap-4 gap-x-8 gap-y-3 md:justify-start">
-                    {/* color */}
-                    <div className="lg:w-[34%] flex items-center lg:gap-3 md:gap-2 gap-1">
-                        <p className="lg:text-lg font-semibold">Color: </p>
-                        <div className="flex flex-wrap gap-2">
-                            {
-                                product?.color?.map(
-                                    eachColor => (
-                                        <span
-                                            key={eachColor}
-                                            className={`lg:w-6 lg:h-6 w-4 h-4 rounded-full ${selectedColor === eachColor?.colorName ? ' border-2 border-purple-800' : 'border border-gray-300'}`}
-                                            style={{ backgroundColor: eachColor?.colorValue }}
-                                            title={eachColor?.colorName}
-                                            onClick={() => setSelectedColor(eachColor?.colorName)}
-                                        />
-                                    )
-                                )
-                            }
+        <div>
+            {
+                error && <p className="lg:text-2xl/relaxed md:text-xl/relaxed text-lg/relaxed lg:my-44 md:my-36 my-28 text-black text-center font-semibold">{error}</p>
+            }
+            {
+                loading ?
+                    (
+                        <p className="lg:text-2xl/relaxed md:text-xl/relaxed text-lg/relaxed lg:my-44 md:my-36 my-28 text-black text-center font-semibold">Loading...</p>
+                    ) : (
+
+                        <div className="flex md:flex-row flex-col items-center lg:gap-8 md:gap-7 gap-6 lg:px-20 md:px-12 px-6 lg:mt-5 md:mt-4 mt-3">
+                            <div className="lg:w-[40%] md:w-[35%]">
+                                <img className="lg:h-96 md:h-64 h-52" src={product?.image} alt="product image" />
+                            </div>
+                            <div className="lg:w-[60%] md:w-[65%]">
+                                <div>
+                                    <h2 className="lg:text-4xl/normal md:text-3xl/normal text-2xl/normal font-bold text-black">{product?.title}</h2>
+                                    <p className="lg:mt-3 md:mt-2 mt-1 lg:text-lg/normal text-justify">{product?.desc}</p>
+                                    <h3 className="lg:text-4xl md:text-3xl text-2xl font-semibold text-black lg:mt-7 md:mt-5 mt-4">$ {product?.price}</h3>
+                                </div>
+                                <div className="lg:mt-7 md:mt-5 mt-4 flex md:flex-row md:flex-nowrap flex-wrap lg:gap-16 md:gap-4 gap-x-8 gap-y-3 md:justify-start">
+                                    {/* color */}
+                                    <div className="lg:w-[34%] flex items-center lg:gap-3 md:gap-2 gap-1">
+                                        <p className="lg:text-lg font-semibold">Color: </p>
+                                        <div className="flex flex-wrap gap-2">
+                                            {
+                                                product?.color?.map(
+                                                    eachColor => (
+                                                        <span
+                                                            key={eachColor}
+                                                            className={`lg:w-6 lg:h-6 w-4 h-4 rounded-full ${selectedColor === eachColor?.colorName ? ' border-2 border-purple-800' : 'border border-gray-300'}`}
+                                                            style={{ backgroundColor: eachColor?.colorValue }}
+                                                            title={eachColor?.colorName}
+                                                            onClick={() => setSelectedColor(eachColor?.colorName)}
+                                                        />
+                                                    )
+                                                )
+                                            }
+                                        </div>
+                                    </div>
+                                    {/* size */}
+                                    <div className="flex items-center lg:gap-3 md:gap-2 gap-1">
+                                        <h4 className="lg:text-lg text-base font-semibold">Size:</h4>
+                                        <select
+                                            className="lg:py-2 md:py-[4px] py-[3px] lg:px-4 md:px-2 px-1 lg:text-lg text-base font-medium rounded-lg lg:w-28 md:w-16 w-20 border-2 border-[#787878]"
+                                            value={selectedSize}
+                                            onChange={(e) => setSelectedSize(e.target.value)}
+                                        >
+                                            {
+                                                product?.size?.map(eachSize => (
+                                                    <option key={eachSize} value={eachSize}>{eachSize}</option>
+                                                ))
+                                            }
+                                        </select>
+                                    </div>
+                                    {/* quantity */}
+                                    <div className="flex items-center lg:gap-3 md:gap-2 gap-1">
+                                        <p className="lg:text-lg font-semibold">Quantity: </p>
+                                        <div className="flex items-center lg:text-xl md:text-lg text-base font-semibold lg:gap-3 md:gap-[7px] gap-2">
+                                            <h4 className="border-2 border-[#787878] bg-white lg:p-2 p-1 rounded-lg" onClick={handleDecreaseProduct}><FaMinus className="lg:text-sm text-xs" /></h4>
+                                            <h4 className="text-purple-700 font-semibold">{productQuantity}</h4>
+                                            <h4 className="border-2 border-[#787878] bg-white lg:p-2 p-1 rounded-lg" onClick={handleIncreaseProduct}><FaPlus className="lg:text-sm text-xs" /></h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex lg:gap-4 md:gap-3 gap-2 lg:mt-8 md:mt-7 mt-6">
+                                    <button className="lg:py-3 md:py-[10px] py-2 border-2 rounded-2xl bg-gradient-to-r from-blue-600 to-purple-800 text-white font-semibold lg:text-lg text-base lg:w-44 md:w-40 w-36" onClick={handleAddToCart}>Add to Cart</button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    {/* size */}
-                    <div className="flex items-center lg:gap-3 md:gap-2 gap-1">
-                        <h4 className="lg:text-lg text-base font-semibold">Size:</h4>
-                        <select
-                            className="lg:py-2 md:py-[4px] py-[3px] lg:px-4 md:px-2 px-1 lg:text-lg text-base font-medium rounded-lg lg:w-28 md:w-16 w-20 border-2 border-[#787878]"
-                            value={selectedSize}
-                            onChange={(e) => setSelectedSize(e.target.value)}
-                        >
-                            {
-                                product?.size?.map(eachSize => (
-                                    <option key={eachSize} value={eachSize}>{eachSize}</option>
-                                ))
-                            }
-                        </select>
-                    </div>
-                    {/* quantity */}
-                    <div className="flex items-center lg:gap-3 md:gap-2 gap-1">
-                        <p className="lg:text-lg font-semibold">Quantity: </p>
-                        <div className="flex items-center lg:text-xl md:text-lg text-base font-semibold lg:gap-3 md:gap-[7px] gap-2">
-                            <h4 className="border-2 border-[#787878] bg-white lg:p-2 p-1 rounded-lg" onClick={handleDecreaseProduct}><FaMinus className="lg:text-sm text-xs" /></h4>
-                            <h4 className="text-purple-700 font-semibold">{productQuantity}</h4>
-                            <h4 className="border-2 border-[#787878] bg-white lg:p-2 p-1 rounded-lg" onClick={handleIncreaseProduct}><FaPlus className="lg:text-sm text-xs" /></h4>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex lg:gap-4 md:gap-3 gap-2 lg:mt-8 md:mt-7 mt-6">
-                    <button className="lg:py-3 md:py-[10px] py-2 border-2 rounded-2xl bg-gradient-to-r from-blue-600 to-purple-800 text-white font-semibold lg:text-lg text-base lg:w-44 md:w-40 w-36" onClick={handleAddToCart}>Add to Cart</button>
-                </div>
-            </div>
+                    )
+            }
         </div>
     );
 };
