5eee7e410662fafe063ffa72b5fd90e3276183d9
Shaily Sarker <shailysarker99@gmail.com>
2026-02-27 23:55:02 +0600

    base url updated

--- src/helpers/config/envConfig.js
+++ src/helpers/config/envConfig.js
@@ -1,2 +1,1 @@
-export const BASE_URL = "https://stylish-fashion-server-side.onrender.com/api/v1";
-// export const BASE_URL = "http://localhost:3000/api/v1";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL
